db7a493d3920de0ac117cfa84afd22b32473e647
Eduardo Portillo <eportillovirreira@gmail.com>
2026-06-08 22:37:52 -0400

    update guard fix tmux copy

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,11 @@
 -- Add any additional options here
 
 vim.opt.runtimepath:prepend(vim.fn.stdpath("data") .. "/site")
+
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy  = { ["+"] = "wl-copy", ["*"] = "wl-copy --primary" },
+  paste = { ["+"] = "wl-paste --no-newline", ["*"] = "wl-paste --no-newline --primary" },
+  cache_enabled = 0,
+}
+vim.opt.clipboard = "unnamedplus"
